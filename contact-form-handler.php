e46914f23b2fe8120811236c9f0abc176fdaa464
Nikhitha Sakhamuri <30761989+nikhsakhamuri@users.noreply.github.com>
2019-11-02 17:30:59 -0700

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,25 +1,22 @@
-
 <?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $visitor_number = $_POST['phone'];
+    $message = $_POST['message'];
 
-if (isset($_POST['submit']))
-{
-    $name=$_POST['name'];
-    $phone=$_POST['phone'];
-    $mailFrom= $_POST['mail'];
-    $message= $_POST['message'];
+    $email_from = 'cppactivestab@gmail.com';
+    $email_subject = "New Form Submission";
+     $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Number: $visitor_number.\n".
+                            "User Message: $message.\n";
     
-    $mailTo="nikhsakhamuri@gmail.com";
-    $headers= "From: ".$name;
-    $txt= " You have received an email from ".$name.".\n\n ".$message;
-    
-    mail($mailTo,$subject, $txt, $headers);
-    header(" Location: index.php?mailsend");
-}
-    
- 
+    $to = "natlho9562@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers = "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: sponsorship.html");
     
 
 
 ?>
-
-
